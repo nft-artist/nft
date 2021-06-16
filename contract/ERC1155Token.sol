acdb4f463ad16c65d1e4a16e21163809e75f2620
artist <nftartist@163.com>
2021-06-16 16:01:53 +0800

    add pinks

--- contract/ERC1155Token.sol
+++ contract/ERC1155Token.sol
@@ -1181,16 +1181,68 @@ library Strings {
 }
 
 /**
+ * @dev Implementation of royalties for 721s
+ * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-2981.md
+ */
+interface IERC2981 {
+    // ERC165 bytes to add to interface array - set in parent contract
+    // implementing this standard
+    //
+    // bytes4(keccak256("royaltyInfo(uint256)")) == 0xcef6d368
+    // bytes4(keccak256("onRoyaltiesReceived(address,address,uint256,address,uint256,bytes32)")) == 0xe8cb9d99
+    // bytes4(0xcef6d368) ^ bytes4(0xe8cb9d99) == 0x263d4ef1
+    // bytes4 private constant _INTERFACE_ID_ERC721ROYALTIES = 0x263d4ef1;
+    // _registerInterface(_INTERFACE_ID_ERC721ROYALTIES);
+
+    // @notice Called to return both the creator's address and the royalty percentage
+    // @param _tokenId - the NFT asset queried for royalty information
+    // @return receiver - address of who should be sent the royalty payment
+    // @return amount - a percentage calculated as a fixed point
+    //         with a scaling factor of 100000 (5 decimals), such that
+    //         100% would be the value 10000000, as 10000000/100000 = 100.
+    //         1% would be the value 100000, as 100000/100000 = 1
+    function royaltyInfo(uint256 _tokenId) external view returns (address receiver, uint256 amount);
+
+    // @notice Called when royalty is transferred to the receiver. This
+    //         emits the RoyaltiesReceived event as we want the NFT contract
+    //         itself to contain the event for easy tracking by royalty receivers.
+    // @param _royaltyRecipient - The address of who is entitled to the
+    //                            royalties as specified by royaltyInfo().
+    // @param _buyer - If known, the address buying the NFT on a secondary
+    //                 sale. 0x0 if not known.
+    // @param _tokenId - the ID of the ERC-721 token that was sold
+    // @param _tokenPaid - The address of the ERC-20 token used to pay the
+    //                     royalty fee amount. Set to 0x0 if paid in the
+    //                     native asset (ETH).
+    // @param _amount - The amount being paid to the creator using the
+    //                  correct decimals from _tokenPaid's ERC-20 contract
+    //                  (i.e. if 7 decimals, 10000000 for 1 token paid)
+    // @param _metadata - Arbitrary data attached to this payment
+    // @return `bytes4(keccak256("onRoyaltiesReceived(address,address,uint256,address,uint256,bytes32)"))`
+    function onRoyaltiesReceived(address _royaltyRecipient, address _buyer, uint256 _tokenId, address _tokenPaid, uint256 _amount, bytes32 _metadata) external returns (bytes4);
+
+    // @dev This event MUST be emitted by `onRoyaltiesReceived()`.
+    event RoyaltiesReceived(
+        address indexed _royaltyRecipient,
+        address indexed _buyer,
+        uint256 indexed _tokenId,
+        address _tokenPaid,
+        uint256 _amount,
+        bytes32 _metadata
+    );
+
+}
+
+/**
  * @title ERC1155Tradable
  * ERC1155Tradable - ERC1155 contract that whitelists an operator address, 
  * has create and mint functionality, and supports useful standards from OpenZeppelin,
   like _exists(), name(), symbol(), and totalSupply()
  */
-contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable, MinterRole, WhitelistAdminRole {
+contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable, MinterRole, WhitelistAdminRole, IERC2981 {
 	using Strings for string;
 
 	uint256 private _currentTokenID = 0;
-	mapping(uint256 => address) public creators;
 	mapping(uint256 => uint256) public tokenSupply;
 	mapping(uint256 => uint256) public tokenMaxSupply;
 	// Contract name
@@ -1200,6 +1252,25 @@ contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable, 
 
     mapping(uint256 => string) private uris;
 
+
+  /// @notice Called to return both the creator's address and the royalty percentage
+  /// @param _tokenId - the NFT asset queried for royalty information
+  /// @return receiver - address of who should be sent the royalty payment
+  /// @return amount - a percentage calculated as a fixed point
+  ///         with a scaling factor of 100000 (5 decimals), such that
+  ///         100% would be the value 10000000, as 10000000/100000 = 100.
+  ///         1% would be the value 100000, as 100000/100000 = 1
+  struct RoyaltyInfo {
+      address creator;
+      uint256 amount;
+  }
+
+  uint256 public _defultRoyalty = 1000000; // 10%
+  mapping(uint256 => RoyaltyInfo) private _royaltyInfos;
+
+  event CreatorChanged(uint256 indexed _id, address indexed _creator);
+  event DefultRoyaltyChanged(uint256 _royalty);
+    
     bool private constructed = false;
 
     function init(
@@ -1289,7 +1360,9 @@ contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable, 
 		require(_initialSupply <= _maxSupply, "Initial supply cannot be more than max supply");
 		uint256 _id = _getNextTokenID();
 		_incrementTokenTypeId();
-		creators[_id] = msg.sender;
+    
+    _royaltyInfos[_id].creator = msg.sender;
+    _royaltyInfos[_id].amount = _defultRoyalty;
 
 		if (bytes(_uri).length > 0) {
 		    uris[_id] = _uri;
@@ -1305,6 +1378,37 @@ contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable, 
 		return _id;
 	}
 	
+  modifier creatorOnly(uint256 _tokenId) {
+      require(
+          _royaltyInfos[_tokenId].creator == msg.sender, 
+          "MintMulTRSRNFT: ONLY_CREATOR_ALLOWED"
+      );
+      _;
+  }
+
+  function modifyRoyalty(uint256 _id ,uint256 amount) external creatorOnly(_id) {
+      _royaltyInfos[_id].amount = amount;
+  }
+
+  function setCreator(uint256 _id, address _to) public creatorOnly(_id) {
+      require(
+          _to != address(0),
+          "MintMulTRSRNFT: INVALID_ADDRESS."
+      );
+      _royaltyInfos[_id].creator = _to;
+      emit CreatorChanged(_id, _to);
+  }
+
+  function royaltyInfo(uint256 _id) external view returns (address receiver, uint256 amount) {
+      receiver = _royaltyInfos[_id].creator;
+      amount = _royaltyInfos[_id].amount;
+  }
+
+  function onRoyaltiesReceived(address _royaltyRecipient, address _buyer, uint256 _tokenId, address _tokenPaid, uint256 _amount, bytes32 _metadata) external  returns (bytes4) {
+    emit RoyaltiesReceived(_royaltyRecipient, _buyer, _tokenId, _tokenPaid, _amount, _metadata);    
+    return bytes4(keccak256("onRoyaltiesReceived(address,address,uint256,address,uint256,bytes32)"));
+  }
+
 	function updateUri(uint256 _id, string calldata _uri) external onlyWhitelistAdmin{
 	    if (bytes(_uri).length > 0) {
 		    uris[_id] = _uri;
@@ -1344,7 +1448,7 @@ contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable, 
 	 * @return bool whether the token exists
 	 */
 	function _exists(uint256 _id) internal view returns (bool) {
-		return creators[_id] != address(0);
+    return _royaltyInfos[_id].creator != address(0);
 	}
 
 	/**
@@ -1367,8 +1471,8 @@ contract ERC1155Token is ERC1155Tradable {
     
     string private _contractURI = "https://github.com/nft-artist/nft";
     
-	constructor() public ERC1155Tradable("xxxx", "xxxx") {
-		_setBaseMetadataURI("https://raw.githubusercontent.com/nft-artist/nft/master/chr/");
+	constructor() public ERC1155Tradable("ScRaTcH_P", "ScRaTcH_P") {
+		_setBaseMetadataURI("https://raw.githubusercontent.com/nft-artist/nft/master/rapper/");
 	}
 
 	function contractURI() public view returns (string memory) {
